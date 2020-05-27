8f56f1a95a7fc9319268fe37ec0f2f3026ffe17f
Matheus Feola <matheusfeola@gmail.com>
2020-05-27 14:14:58 -0300

    Add push alias

--- config/ios-configuration.sh
+++ config/ios-configuration.sh
@@ -1,6 +1,7 @@
 # Automatic download Xcode?
 
 alias update="gfa ; ggpull"
+alias push="gpsup"
 alias rmderived="rm -rf ~/Library/Developer/Xcode/DerivedData"
 alias podi="bundle exec pod install"
 alias podiup="bundle exec pod install --repo-update"

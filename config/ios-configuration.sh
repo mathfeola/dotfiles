8e834e1511f6a8c258d6af53bf30ff0c85538803
Matheus Feola <matheusfeola@gmail.com>
2020-05-27 10:49:03 -0300

    Add iOS related aliases

--- config/ios-configuration.sh
+++ config/ios-configuration.sh
@@ -1,2 +1,9 @@
 # Automatic download Xcode?
+
+alias update="gfa ; ggpull"
+alias rmderived="rm -rf ~/Library/Developer/Xcode/DerivedData"
+alias podi="bundle exec pod install"
+alias podiup="bundle exec pod install --repo-update"
+alias repoup="bundle exec pod repo update"
+alias bi="bundle install"
 brew install chisel

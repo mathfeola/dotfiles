d5162efaf386af6e9d9d05be63a08c15613d94fa
Matheus Feola <matheusfeola@gmail.com>
2020-09-20 13:31:20 -0300

    improving initial configs

--- installSoftwares.sh
+++ installSoftwares.sh
@@ -72,3 +72,7 @@ defaults write com.apple.dock static-only -bool true; killall Dock
 defaults write com.apple.Dock showhidden -bool yes; killall Dock
 # Remove autohide animation
 defaults write com.apple.dock autohide-delay -float 0; killall Dock
+# Adding ‘Quit’ option to Finder on a Mac
+defaults write com.apple.finder QuitMenuItem -bool true; killall Finder
+# Stop Photos from opening automatically on your Mac
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true

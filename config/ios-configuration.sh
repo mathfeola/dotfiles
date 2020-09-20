0286c9154ef6669655fc4a23d1830d88b8da3dcb
Matheus Feola <matheusfeola@gmail.com>
2020-09-20 13:23:07 -0300

    Update iOS script

--- config/ios-configuration.sh
+++ config/ios-configuration.sh
@@ -1,2 +1,13 @@
-# Automatic download Xcode?
+#!/bin/bash
+
+# instal Xcode
+mas install 497799835
 brew install chisel
+# TODO: alias to automatically create .lldbinit
+
+echo '# iOS related aliases' >> ~/.zshrc
+echo 'alias repoup="bundle exec pod repo update"' >> ~/.zshrc
+echo 'alias podi="bundle exec pod install"' >> ~/.zshrc
+echo 'alias bi="bundle install"' >> ~/.zshrc
+echo 'alias podiup="bundle exec pod install --repo-update"' >> ~/.zshrc
+echo 'alias rmderived="rm -rf ~/Library/Developer/Xcode/DerivedData"' >> ~/.zshrc

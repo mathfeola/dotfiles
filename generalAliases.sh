1d81b8ecf9c2c1e95f6ded7020e674439cda6c09
Matheus Feola <matheus.feola@mercadolivre.com>
2020-09-20 21:15:14 -0300

    Move config file

--- generalAliases.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-source config/colors.sh
-
-echo "# General aliases" >> ~/.zshrc
-echo 'alias dl="cd ~/Downloads"' >> ~/.zshrc
-echo 'alias update="gfa ; ggpull"' >> ~/.zshrc
-echo 'alias push="gpsup"' >> ~/.zshrc

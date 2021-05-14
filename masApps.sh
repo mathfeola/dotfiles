bbcbf352a25c22120e23225ac1008888771b73cc
Matheus Feola <matheus.feola@gmail.com>
2021-05-14 20:32:54 -0300

    Remove mainStage

--- masApps.sh
+++ masApps.sh
@@ -14,6 +14,6 @@ mas install 970246631
 # Logic Pro x
 mas install 634148309
 # MainStage 3
-mas install 634159523
+# mas install 634159523
 # Rekordbox
 # wget https://cdn.rekordbox.com/files/20200908113450/Install_rekordbox_6_1_0.pkg_.zip

a98b18902a869576a2812e4b43236d5f7927b179
Matheus Feola <matheusfeola@gmail.com>
2021-06-07 21:19:28 -0300

    Add kindle download to deafult configuration

--- masApps.sh
+++ masApps.sh
@@ -1,19 +1,26 @@
 #!/bin/sh
 
 source config/colors.sh
-## outside installed apps ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 
 # Disk Cleaner
 mas install 970246631
-# Yellow Duck
-# OBS
-# Postman
-# Veracrypt
+
+# Kindle
+mas install 405399194
+
+## outside installed apps ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# Rekordbox
+# https://rekordbox.com/en/download/
+
+# VeraCrypt
+https://github.com/veracrypt/VeraCrypt
 
 ## Music ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 # Logic Pro x
 mas install 634148309
+
 # MainStage 3
 # mas install 634159523
-# Rekordbox
-# wget https://cdn.rekordbox.com/files/20200908113450/Install_rekordbox_6_1_0.pkg_.zip
+

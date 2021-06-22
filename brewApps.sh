30817e8c6a4ef149fcd85bc544a946f6fb62ee5a
Matheus Feola <matheusfeola@gmail.com>
2021-06-21 21:45:34 -0300

    Removing unused brew apps

--- brewApps.sh
+++ brewApps.sh
@@ -3,11 +3,8 @@
 source config/colors.sh
 
 msg_install "Installing apps with brew"
-brew install zsh
 brew install vim
 brew install mas
 brew install unar
 brew install cmatrix
-brew install gh
-brew install youtube-dl
 brew install python
